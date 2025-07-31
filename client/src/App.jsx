946f7f932aef081a96d54d98bfb89b1e7be37cdd
komal-bendresh <komalbendresh1134@gmail.com>
2025-07-31 19:53:15 +0530

    addd match controlller and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./pages/AdminDashboard";
 import SurveyPage from "./pages/SurveyPage";
-import { Routes, Route } from "react-router-dom";
+
 import MatchResultPage from "./pages/MatchResultPage";
 
 function App() {
