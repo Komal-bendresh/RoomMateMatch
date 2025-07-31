18131b6786ec316df65a2b99ec73876715a5589a
nency-p <patnency07@gmail.com>
2025-07-31 18:17:42 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SurveyPage from "./pages/SurveyPage";
+import { Routes, Route } from "react-router-dom";
+import MatchResultPage from "./pages/MatchResultPage";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<SurveyPage />} />
         {/* Add more pages here later like /login, /matches etc. */}
+        <Route path="/result" element={<MatchResultPage />} />
+        {/* other routes */}
       </Routes>
     </Router>
   );
